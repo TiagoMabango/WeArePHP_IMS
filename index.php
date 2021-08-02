8e5e967352a0d466842aeb229b60f8a0eaa27fc7
Tiago Mabango <tiagomabango@gmail.com>
2021-08-02 11:54:08 +0100

    Teste do IMS

---
+++ index.php
@@ -0,0 +1,13 @@
+
+<?php
+
+require __DIR__ . "/Assets/Sweetalert.php";
+require __DIR__ . "/source/autoload.php";
+
+$message = new  \Source\Support\Message();
+
+
+  echo $message->warning("Essa é uma mensagem de teste do IMS");
+//   echo $message->info("Essa é uma mensagem de teste do IMS");
+//echo $message->success("Essa é uma mensagem de teste do IMS");
+//  echo $message->error("Essa é uma mensagem de teste do IMS");
---
+++ source/Support/Message.php
@@ -0,0 +1,134 @@
+<?php
+ 
+use Source\Assets\Sweetalert;
+
+namespace Source\Support;
+
+
+/**
+ * MIXTAPE WE ARE PHP
+ *
+ * @author Tigao Mabango 
+ * @package Source\Support
+ */
+class Message  
+{
+    /** @var string */
+    private $text;
+
+    /** @var string */
+    private $type;
+
+    /** @var string */
+    private $before;
+
+    /** @var string */
+    private $after;
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->render();
+    }
+
+    /**
+     * @return string
+     */
+    public function getText(): ?string
+    {
+        return $this->before.$this->text.$this->after;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param string $text
+     * @return Message
+     */
+    public function before(string $text): Message
+    {
+        $this->before =$text;
+        return $this;
+    }
+
+    /**
+     * @param string $text
+     * @return Message
+     */
+    public function after(string $text): Message
+    {
+        $this->after = $text;
+        return $this;
+    }
+
+    /**
+     * @param string $message
+     * @return Message
+     */
+    public function info(string $message): Message
+    {
+        $this->type = "info";
+        $this->text = $this->filter($message);
+        return $this;
+    }
+
+    /**
+     * @param string $message
+     * @return Message
+     */
+    public function success(string $message): Message
+    {
+        $this->type = "success";
+        $this->text = $this->filter($message);
+        return $this;
+    }
+
+    /**
+     * @param string $message
+     * @return Message
+     */
+    public function warning(string $message): Message
+    {
+        $this->type = "warning";
+        $this->text = $this->filter($message);
+        return $this;
+    }
+
+    /**
+     * @param string $message
+     * @return Message
+     */
+    public function error(string $message): Message
+    {
+        $this->type = "error";
+        $this->text = $this->filter($message);
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function render(): string
+    {
+        return  "<script>swal('{$this->getText()}' , '', '{$this->getType()}');</script>";
+
+        
+    }
+    
+    /**
+     * @param string $message
+     * @return string
+     */
+    private function filter(string $message): string
+    {
+        return filter_var($message, FILTER_SANITIZE_STRIPPED);
+    }
+}
